93545e22256961977d344c9e5035ac4415d7ef50
heySasha <iatsenko.o@donnu.edu.ua>
2017-07-25 21:57:51 +0300

    Add Company model

---
+++ server/models/company.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const _ = require('lodash');
+
+const CompanySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    estimatedEarnings: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    childCompanies: [{
+        name: {
+            type: String,
+            required: true,
+            minlength: 1
+        },
+        estimatedEarnings: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+    }]
+});
+
+CompanySchema.methods.toJSON = function () {
+    const company = this;
+    const companyObject = company.toObject();
+
+    const estimatedEarningsChildCompanies = companyObject.childCompanies
+        .reduce((sum, company) => sum + company.estimatedEarnings);
+
+    return Object.assign({},
+        _.pick(companyObject, ['_id', 'name', 'estimatedEarnings', 'childCompanies']),
+        {'estimatedEarningsAndEstimatedEarningsChildCompanies':
+            companyObject.estimatedEarnings + estimatedEarningsChildCompanies });
+};
+
+const Company = mongoose.model('Company', CompanySchema);
+
+module.exports = { Company };
